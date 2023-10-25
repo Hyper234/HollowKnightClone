99aa7190ce815557746fe40407340fcb88921e70
20451 <radek.kierski@gmail.com>
2023-10-25 21:29:38 +0200

    Separated vertical and horizontal camera follow speed and adjusted values

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    [SerializeField] private float smoothSpeed;
+    [SerializeField] private float horizontalSmoothSpeed;
+    [SerializeField] private float verticalSmoothSpeed;
     [SerializeField] private Vector3 cameraOffset;
     [SerializeField] private float cameraDistance;
 
     private void FixedUpdate()
     {
         Vector3 desiredPosition = target.position + cameraOffset + new Vector3(cameraDistance * target.localScale.x, 0, 0);
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        Vector3 smoothedPosition = 
+            new Vector3(Mathf.Lerp(transform.position.x, desiredPosition.x, horizontalSmoothSpeed),
+            Mathf.Lerp(transform.position.y, desiredPosition.y, verticalSmoothSpeed),
+            -1);
         transform.position = smoothedPosition;
     }
 }

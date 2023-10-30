6b4bf34fdee9cb123c328529a8b9b84b0e79dd3d
20451 <radek.kierski@gmail.com>
2023-10-30 10:35:09 +0100

    Added looking up and down using W and S
    - created new input actions for camera controls
    - created new camera script
    - added public cameraControlsOffset variable in camera follow script

---
+++ Assets/Scripts/Camera/CameraControls.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+    [SerializeField] float lookDistance;
+
+    private CameraFollow cameraFollow;
+    private CameraInputActions cameraInputActions;
+
+    private bool isLookHeld = false;
+
+    void Awake()
+    {
+        cameraFollow = GetComponent<CameraFollow>();
+
+        cameraInputActions = new CameraInputActions();
+        cameraInputActions.Camera.Enable();
+        cameraInputActions.Camera.LookUpDown.performed += LookUpDown_performed;
+    }
+
+    private void LookUpDown_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        float input = cameraInputActions.Camera.LookUpDown.ReadValue<float>();
+
+        if (!isLookHeld)
+        {
+            cameraFollow.cameraControlsOffset += input * lookDistance;
+        }
+        else
+        {
+            cameraFollow.cameraControlsOffset = 0;
+        }
+
+        isLookHeld = !isLookHeld;
+    }
+}
--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -8,9 +8,11 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private Vector3 cameraOffset;
     [SerializeField] private float cameraDistance;
 
+    public float cameraControlsOffset = 0;
+
     private void FixedUpdate()
     {
-        Vector3 desiredPosition = target.position + cameraOffset + new Vector3(cameraDistance * target.localScale.x, 0, 0);
+        Vector3 desiredPosition = target.position + cameraOffset + new Vector3(cameraDistance * target.localScale.x, cameraControlsOffset, 0);
         Vector3 smoothedPosition = 
             new Vector3(Mathf.Lerp(transform.position.x, desiredPosition.x, horizontalSmoothSpeed),
             Mathf.Lerp(transform.position.y, desiredPosition.y, verticalSmoothSpeed),

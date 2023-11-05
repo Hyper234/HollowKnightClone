b215348956a507400e76267cbfae1074eb167ce5
20451 <radek.kierski@gmail.com>
2023-11-05 11:29:17 +0100

    Cleaning PlayerMovement script
    - added comments
    - changed isGrounded checks to bool variable updated once per frame

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -25,13 +26,14 @@ public class PlayerMovement : MonoBehaviour
     [Header("Collisions")]
     [SerializeField] private LayerMask groundLayerMask;
 
+    private bool isOnWall = false;
+    private bool isGrounded = false;
     private float horizontalInput;
     private bool jumpHeld = true;
     private bool doubleJumpAvailable = true;
     private bool isDashing = false;
     private bool canDash = true;
     private float dashTimer = 1000f;
-    private bool isWallSliding = false;
 
 
     private void Awake()
@@ -54,16 +56,21 @@ public class PlayerMovement : MonoBehaviour
         {
             canDash = true;
             doubleJumpAvailable = true;
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
         }
 
         //wall check
         if (IsOnWall())
         {
-            isWallSliding = true;
+            isOnWall = true;
         }
         else
         {
-            isWallSliding = false;
+            isOnWall = false;
         }
 
         //movement
@@ -74,7 +81,7 @@ public class PlayerMovement : MonoBehaviour
 
         //animations
         animator.SetBool("running", horizontalInput != 0);
-        animator.SetBool("grounded", IsGrounded());
+        animator.SetBool("grounded", isGrounded);
 
         //update timers
         dashTimer += Time.deltaTime;
@@ -82,17 +89,21 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move()
     {
+        //read player input
         horizontalInput = playerInputActions.Player.Movement.ReadValue<float>();
 
-        if (isWallSliding)
+        if (isOnWall)
         {
             if(rigidBody2D.velocity.y > 0)
+                //if the player is jumping against the wall don't wall slide
                 rigidBody2D.velocity = new Vector2(horizontalInput * movementSpeed, rigidBody2D.velocity.y);
             else
+                //if the player is falling against the wall then wall slide
                 rigidBody2D.velocity = new Vector2(horizontalInput * movementSpeed, -wallSlidingSpeed);
         }
         else
         {
+            //default movement
             rigidBody2D.velocity = new Vector2(horizontalInput * movementSpeed, rigidBody2D.velocity.y);
         }
 
@@ -101,12 +112,14 @@ public class PlayerMovement : MonoBehaviour
 
     private void Jump_performed(InputAction.CallbackContext obj)
     {
+        //the player cannot jump while dashing
         if (!isDashing)
         {
-            if (IsGrounded())
+            if (isGrounded)
             {
                 if (jumpHeld)
                 {
+                    //default jump
                     rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, jumpPower);
                     animator.SetTrigger("jump");
                 }
@@ -117,6 +130,7 @@ public class PlayerMovement : MonoBehaviour
                 {
                     if (doubleJumpAvailable)
                     {
+                        //double jump if player not grounded
                         rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, jumpPower);
                         animator.SetTrigger("jump");
                         doubleJumpAvailable = false;
@@ -124,6 +138,7 @@ public class PlayerMovement : MonoBehaviour
                 }
                 else
                 {
+                    //resets player's vertical movement speed when jump released
                     rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, Mathf.Min(0, rigidBody2D.velocity.y));
                 }
             }
@@ -134,28 +149,35 @@ public class PlayerMovement : MonoBehaviour
 
     private void Dash_performed(InputAction.CallbackContext obj)
     {
+        //checking if the player can dash
         if (!isDashing && canDash && dashTimer > dashCooldown)
         {
+            //perform dash
             StartCoroutine(Dash());
         }
     }
 
     private IEnumerator Dash()
     {
+        //set all states and dashing animation
         canDash = false;
         isDashing = true;
         animator.SetBool("dashing", isDashing);
         animator.SetTrigger("dash_init");
 
+        //set dashing movement
         float gravity = rigidBody2D.gravityScale;
         rigidBody2D.gravityScale = 0;
         rigidBody2D.velocity = new Vector2(Mathf.Sign(transform.localScale.x) * dashingSpeed, 0);
 
+        //wait for dash to end
         yield return new WaitForSeconds(dashingTime);
 
+        //reset all movement after the dash
         rigidBody2D.gravityScale = gravity;
         rigidBody2D.velocity = Vector2.zero;
 
+        //stop dashing animation and start dash cooldown timer
         isDashing = false;
         animator.SetBool("dashing", isDashing);
         dashTimer = 0f;
@@ -163,10 +185,14 @@ public class PlayerMovement : MonoBehaviour
 
     private void FlipSprite()
     {
-        if (horizontalInput * transform.localScale.x < 0)
-            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        //flip the sprite depending on the horizontal velocity
+        if (rigidBody2D.velocity.x < 0)
+            transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+        else
+            transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
     }
 
+    //checking if the player is touching the ground
     private bool IsGrounded()
     {
         float margin = .02f;
@@ -174,6 +200,7 @@ public class PlayerMovement : MonoBehaviour
         return Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, margin, groundLayerMask).collider != null;
     }
 
+    //checking if the player is touching a wall
     private bool IsOnWall()
     {
         float margin = .02f;
